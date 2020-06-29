910b081c1fa6bc56d47b94ca29df24ae361b0c0f
sasz94 <sacha.bartlomiej@gmail.com>
2020-06-29 21:11:04 +0200

    Configure store and create  budget and common reducer

---
+++ src/data/constants/index.js
@@ -0,0 +1,16 @@
+
+
+export * from './budget.constants'
+export * from './common.constants'
+
+
+export const LOADING_STATES = {
+    INITIAL: 'INITIAL',
+    LOADING: 'LOADING',
+    LOADED: 'LOADED',
+    FAILED: 'FAILED',
+}
+
+export default {
+    LOADING_STATES,
+}
